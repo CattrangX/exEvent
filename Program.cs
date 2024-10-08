392740ce4e726a36c61e54f8f93a3d1dc762f89a
CattrangX <nhannguyenthanh2305@gmail.com>
2024-10-08 23:49:45 +0700

    hello

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ViduveEvent
+{
+
+    public class Publisher
+    {
+        public event Action<object> event_news;
+
+        public void Send()
+        {
+            event_news?.Invoke("Co tin moi");
+        }
+    }
+
+    public class SubscriberA
+    {
+        public void Sub(Publisher p)
+        {
+            p.event_news += ReceiverFromPublisher;
+        }
+
+        void ReceiverFromPublisher(object data)
+        {
+            Console.WriteLine("SubscriberA: " + data.ToString());
+        }
+    }
+
+    public class SubscriberB
+    {
+        public void Sub(Publisher p)
+        {
+            p.event_news += ReceiverFromPublisher;
+        }
+
+        void ReceiverFromPublisher(object data)
+        {
+            Console.WriteLine("SubscriberB: " + data.ToString());
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Publisher p = new Publisher();
+            SubscriberA sa = new SubscriberA();
+            SubscriberB sb = new SubscriberB();
+
+            sa.Sub(p);
+            sb.Sub(p);
+
+            p.Send();
+        }
+    }
+}
